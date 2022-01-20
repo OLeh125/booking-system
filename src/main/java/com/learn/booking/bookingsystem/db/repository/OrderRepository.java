93be74a3110522840ad8ad1edb9b5afe76c3d0e9
Oleh Artymyak <oleh.artymyak@danfoss.com>
2022-01-20 10:32:35 +0200

    align params in order repository

--- src/main/java/com/learn/booking/bookingsystem/db/repository/OrderRepository.java
+++ src/main/java/com/learn/booking/bookingsystem/db/repository/OrderRepository.java
@@ -15,7 +15,7 @@ public interface OrderRepository extends JpaRepository<Order, Integer> {
     @Transactional(readOnly = true)
     Optional<Order> getByUuid(UUID uuid);
 
-    @Query("select o from Order o JOIN fetch o.client JOIN fetch o.tickets where o.uuid in (:uuid)")
+    @Query("select o from Order o JOIN fetch o.client JOIN fetch o.tickets where o.uuid in (:uuids)")
     @Transactional(readOnly = true)
     List<Order> findAllByUuid(List<UUID> uuids);
 
